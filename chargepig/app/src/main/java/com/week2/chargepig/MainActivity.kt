77eaa2feceb93cc602a837e10ca96b5816b30b1d
박진성 <plashdof@gmail.com>
2022-12-27 17:51:33 +0900

    qr 스캔 성공

--- chargepig/app/src/main/java/com/week2/chargepig/MainActivity.kt
+++ chargepig/app/src/main/java/com/week2/chargepig/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 import com.week2.chargepig.databinding.ActivityMainBinding
@@ -27,42 +29,40 @@ class MainActivity : AppCompatActivity() {
         binding.mainBottomnav.setupWithNavController(navController)
 
         binding.mainBtnQr.setOnClickListener {
+
             val integrator = IntentIntegrator(this)
-            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // 여러가지 바코드중에 특정 바코드 설정 가능
+            integrator.setDesiredBarcodeFormats(ScanOptions.QR_CODE) // 여러가지 바코드중에 특정 바코드 설정 가능
             integrator.setPrompt("손난로 상단의 QR코드를 스캔해주세요") // 스캔할 때 하단의 문구
             integrator.setCameraId(0) // 0은 후면 카메라, 1은 전면 카메라
             integrator.setBeepEnabled(true) // 바코드를 인식했을 때 삑 소리유무
             integrator.setBarcodeImageEnabled(false) // 스캔 했을 때 스캔한 이미지 사용여부
             integrator.captureActivity = QrActivity::class.java
-            integrator.initiateScan() // 스캔
+            activityResult.launch(integrator.createScanIntent()) // 스캔
         }
 
-        val options = ScanOptions()
-        barcodeLauncher.launch(options)
     }
 
-    private val barcodeLauncher = registerForActivityResult(ScanContract()){result->
 
-        if(result.contents != null){
-            Toast.makeText(this@MainActivity, "${result.contents}", Toast.LENGTH_SHORT).show()
-        }
-    }
+    private val activityResult = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        val data = result.data
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val intentResult: IntentResult? = IntentIntegrator.parseActivityResult(result.resultCode, data)
+        if(intentResult != null){
 
-        if (result != null) {
-            if (result.contents != null) {
-                Log.e("INFO: ", "${result.contents}")
-            } else {
-                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show()
+            //QRCode Scan 성공
+            if(intentResult.contents != null){
+                //QRCode Scan result 있는경우
+                Toast.makeText(this@MainActivity, "인식된 QR-data: ${intentResult.contents}", Toast.LENGTH_SHORT).show()
+            }else{
+                //QRCode Scan result 없는경우
+                Toast.makeText(this@MainActivity, "인식된 QR-data가 없습니다.", Toast.LENGTH_SHORT).show()
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
+        }else{
+            //QRCode Scan 실패
+            Toast.makeText(this@MainActivity, "QR스캔에 실패했습니다.", Toast.LENGTH_SHORT).show()
         }
+
     }
-
-
 
 
 
