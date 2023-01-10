d8cf1f030e33f879fb9c49924ca3f4ab61aac6d8
박진성 <plashdof@gmail.com>
2023-01-10 21:32:08 +0900

    viewpager 추가

--- app/src/main/java/com/week2/chargepig/view/HomeFragment.kt
+++ app/src/main/java/com/week2/chargepig/view/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.week2.chargepig.view
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.week2.chargepig.R
 import com.week2.chargepig.databinding.FragmentHomeBinding
+import com.week2.chargepig.view.adapter.HomeVPAdapter
 
 class HomeFragment : Fragment(){
     private lateinit var binding : FragmentHomeBinding
@@ -34,6 +38,12 @@ class HomeFragment : Fragment(){
         binding.btnEchopoint.setOnClickListener {
             navController.navigate(R.id.action_homeFragment_to_echopointFragment)
         }
+        val datas = arrayListOf<Int>(R.drawable.test, R.drawable.test2, R.drawable.test3)
+
+        val adapter = HomeVPAdapter(datas)
+        binding.viewpager.adapter = adapter
+
+
 
     }
 
